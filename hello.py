05032f444dd83d387a996e50ec569d65e97eb8b1
Kelvin-Water <kelvinstephen779@gmail.com>
2026-06-04 10:56:51 +0100

    my first push

--- hello.py
+++ hello.py
@@ -1,3 +1,59 @@
-# print("hello, world")
-var1 = "obi is a boy"
-print(var1)
+#varl = "obi"
+#print(type(varl))
+
+
+# var1 = "obi is a life"
+# var2 = "a"
+# print((var1))
+# print(ord(var2))
+# var1 = 25.5
+# x = str(var1)
+# print(x)
+# print(type(x))
+
+# A = 1,2,2,2,3,3,4,4,5,5
+# Z= A,B,C,D,F,E,R,T,Y,U
+
+# x = y = z = "orange"
+# print(x)
+# print(y)
+# print(z)
+
+# x,y,z = "orange", "apple", "grape"
+# print(x)
+# print(y)
+# print(z)
+
+# var3 = "kelvin"
+
+
+# print(len(var3))
+
+var3 = " obi is a boy "
+# print(len(var3))
+
+# print("boy" in var3)
+
+# state = "aba", "enugu", "akwa", "uyo"
+# number = 1, 2, 3, 4, 6, 
+# result = dict(zip(state, number))
+
+# print(result)
+
+# var3 = "obi is a boy"
+# print(var3[1:8:2])
+
+# var4 = "informationtechnology"
+# print(var4[1::3])
+
+# list = ["orange" , "apple", "grape", "cucumber", "banana", "mango", "guava" "berry"]
+# print(list[2][3])
+
+# print(var3.lower())
+# print(var3.capitalize())
+# print(var3.strip())
+
+# A = "obi"
+# B = " ada"
+# z = A + B
+# print(z)
